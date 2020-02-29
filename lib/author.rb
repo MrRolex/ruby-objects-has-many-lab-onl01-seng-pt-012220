ccc824b535aedc99b1f7edd98f6d3eb2dc8ce8fb
Edward Williams <eddie_w38@yahoo.com>
2020-02-29 10:34:25 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,31 +1,27 @@
-class Author 
-  attr_accessor :name
-  @@post_count = 0
+class Author
+
+  attr_accessor :name, :posts
 
   def initialize(name)
     @name = name
     @posts = []
   end
 
-  def posts
-    @posts
-  end
-
   def add_post(post)
-    @posts << post
     post.author = self
-    @@post_count += 1
+    @posts << post
   end
 
-  def add_post_by_title(post_title)
-    post = Post.new(post_title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
   end
 
   def self.post_count
-    @@post_count
+    Post.all.length
   end
 
-end
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+end 
