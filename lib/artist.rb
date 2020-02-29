641e13bfcffba9aff6cf4c5259c2a672b3c1c355
Edward Williams <eddie_w38@yahoo.com>
2020-02-29 10:27:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,9 +17,7 @@ class Artist
 
   def add_song_by_name(song_name)
     song = Song.new(song_name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
+   
   end
 
   def self.song_count
