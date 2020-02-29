4e11106ab48b388008a3e4bc4d4d38e56dfb4f41
Edward Williams <eddie_w38@yahoo.com>
2020-02-29 10:34:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,28 @@
 class Artist
-  attr_accessor :name,  :songs
 
+  attr_accessor :name, :songs
 
   def initialize(name)
     @name = name
-  end
-
-  def songs
-    songs.allselect |song|
-    song.artist == artist
+    @songs = []
   end
 
   def add_song(song)
+    @songs << song
     song.artist = self
   end
 
-  def add_song_by_name(song_name)
-    singer = Song.new(song_name)
-    add_song(singer)
-   end
-
-  def self.song_count
-   Song.all.count
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+    @songs << song
   end
 
-end
+  def self.song_count
+    Song.all.length
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+end 
