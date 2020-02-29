e539a8a127d1fa3d2f1de955424d84b21b8c8633
Edward Williams <eddie_w38@yahoo.com>
2020-02-29 10:19:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,16 @@
 class Song
-  attr_accessor :genre, :artist, :name
+  attr_accessor :artist, :name
 
-  def initialize(name, genre)
+  def initialize(name)
     @name = name
-    self.genre = genre
-    genre.add_song(self)
   end
-end
+
+  def artist_name
+    if artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+end 
