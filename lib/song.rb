8a5a517f470b6769374c38653da776b0b3ae98c0
Edward Williams <eddie_w38@yahoo.com>
2020-02-29 10:23:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,13 @@
 class Song
   attr_accessor :artist, :name
   
-  @@
+  @@all = []
 
   def initialize(name)
     @name = name
-    @all << self
+    @@all << self
   end
 
-  def artist_name
-    if artist
-      self.artist.name
-    else
-      nil
-    end
-  end
-
-end 
+  def self.all
+   @@all
+   end
